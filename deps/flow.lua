8231edde13cb44fb5d3cd6630c4afaa82d3e17d0
tomatokilla <tomatokillar@gmail.com>
2018-07-27 00:32:28 +0800

    4 sync constructing flow schema ..

--- deps/flow.lua
+++ deps/flow.lua
@@ -1,22 +1,23 @@
 local Object = require('core').Object
 local Box = require('statebox')
 
-local type, next, setmetatable, getmetatable, pairs, rawget, rawset = 
-      type, next, setmetatable, getmetatable, pairs, rawget, rawset
+local type, next, setmetatable, getmetatable, pairs, rawget, rawset, error = 
+      type, next, setmetatable, getmetatable, pairs, rawget, rawset, error
+local insert, remove = table.insert, table.remove
+local format = string.format
 
 --[[
   Flow objects:
-    1. director 
-    2. toggler
-    3. resolve
-    4. state container
+    1. routines = {list1 = {...}, list2 = {...}, ...}
+    2. index
+    3. statebox
 ]]
 local flow = Object:extend()
 
 --[[
   Initialize the flow obj
     caveat:
-      routines must be a list, and sub_routine must be an array which means
+      routines must be a dict, and sub_routine must be an array which means
       no sub_sub_routine:
       schema of routines like this:
       routines = {
@@ -27,67 +28,95 @@ local flow = Object:extend()
         },
         subroutine2 = {
           'foo',
-          'bar',
-          ...
+          'bar'
         }
       }
 ]]
-function flow:initialize(routines, index, statebox)
-  if routines == nil then
-    self.routines = {}
-    self.index    = 1
-    self.statebox = {}
-  elseif index == nil then
-    self.routines = routines
-    self.index    = 1
-    self.statebox = {}
-  elseif state
+function flow:initialize(routines)
+  self.routines = routines or {}
+  self.state = Box.new('FLow', 'status container of flow')
+end
+
+-- Initialize the index of task list, default to 1
+function flow:initIndex(n)
+  self.state:set({index = n or 1})
+end
+
+-- 4 Test: check the schema of routines is valid
+local function isArray(t)
+  if type(t) ~= 'table' then return false end
+  local n = #t
+  for k, v in pairs(t) do
+    if type(k) ~= 'number' then return false end
+    if k > n then return false end
+  end
+  return true
+end
+function flow:checkRoutines()
+  for k, v in pairs(self.routines) do
+    if type(k) ~= 'string' then
+      error('routines must be a dict!')
+    end
+    if not isArray(v) then
+      error(format("subroutine: { %s } isn't an array!"))
+    end
   end
 end
 
-function flow:resetRoutine(list)
+-- Reset Routines
+function flow:resetRoutines(routines)
   if type(list) ~= 'table' or next(list) == nil then
     return
   end
-  self.routine = list
+  self.routines = routines
 end
 
--- insert one step to the routine or subroutine at the given index
-function flow:addStep(subRoutine, index, step)
-  if subRoutine == nil then return end
-  if index == nil then
-    step = subRoutine
-    return insert(self.routine, step)
-  end
+-- insert one step to the list at the given index
+function flow:addStep(list, index, step)
+  if list == nil or index == nil then return end
   if step == nil then
-    step = index
-    return insert(self.routine[subRoutine], step)
+    step  = index
+    index = #list + 1
   end
-  return insert(self.routine[subRoutine], index, step)
+  return insert(self.routines[list], index, step)
 end
 
--- Remove an element by value
+function flow:appendStep(list, step)
+  return self:addStep(list, step)
+end
+
+-- Remove one step from the task list
+-- Caveat: the tbl must be an array
 local function removeByValue(tbl, val)
   if type(tbl) ~= 'table' or next(tbl) == nil or not val then
     return
   end
   for i, v in pairs(tbl) do
     if v == val then
-      tbl[i] = nil
+      return remove(tbl, i)
     end
   end
 end
-
-
-function flow:removeStep(subRoutine, step)
-  if subRoutine == nil then return end
-  if step == nil then
-    step = subRoutine
-    return remove(self.routine)
-  end
+function flow:removeStep(list, step)
+  if step == nil then return end
+  return removeByValue(self.routines[list], step)
 end
 
+function flow:removeAllStep(list)
+  if not list then return end
+  self.routines[list] = {}
+end
 
+-- Resolve index & toggle index
+function flow:resolve(...)
+  -- definition ..
+  self.state:set({})
+end
+
+function flow:toggleIndex()
+  -- toggle flow index according to state
+
+end
 
 
 
@@ -98,9 +127,6 @@ end
 
 
 function flow:run()
-end
-
-function flow:toggler()
 end
 
 
