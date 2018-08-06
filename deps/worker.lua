4f6d38ac6336623cc5482be5487fb2c2a0e6ed48
tomatokilla <tomatokillar@gmail.com>
2018-08-06 23:42:59 +0800

    junkman & worker

---
+++ deps/worker.lua
@@ -0,0 +1,35 @@
+--[[
+  Worker layer of phage
+]]
+
+local Object = require('coer').Object
+
+local type, pairs, next =
+      type, pairs, next
+
+local _M = Object:extend()
+
+function _M:initialize(taskmap)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-----------------------------------------------------
+return _M
